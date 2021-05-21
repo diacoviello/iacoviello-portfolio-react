85d116bc5aee75fa96d176ab12f954b6bc41f02c
Dave <diacovmusic@gmail.com>
2021-05-20 21:51:09 -0400

    pdf resume done

--- src/components/ResumePDF/index.js
+++ src/components/ResumePDF/index.js
@@ -1,24 +1,77 @@
 import React, { useState } from "react";
 import { Document, Page } from "react-pdf";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
-export default function AllPages(props) {
+export default function SinglePage(props) {
   const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
+    setPageNumber(1);
+  }
+
+  function changePage(offset) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
   }
 
   const { pdf } = props;
 
   return (
-    <Document
-      file={pdf}
-      options={{ workerSrc: "/pdf.worker.js" }}
-      onLoadSuccess={onDocumentLoadSuccess}
-    >
-      {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
+    <>
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item xs={1}>
+          <Button
+            variant="outlined"
+            disabled={pageNumber <= 1}
+            onClick={previousPage}
+            style={{ backgroundColor: "#9433A2" }}
+          >
+            &lt; &lt;
+          </Button>
+        </Grid>
+        <Grid item xs={2}>
+          <h3 align="center" marginBottom="0" padding="1">
+            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+          </h3>
+        </Grid>
+        <Grid item xs={1}>
+          <Button
+            variant="outlined"
+            disabled={pageNumber >= numPages}
+            onClick={nextPage}
+            style={{ backgroundColor: "#9433A2" }}
+          >
+            &gt; &gt;
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid container justify="center">
+        <Grid item>
+          <Document
+            file={pdf}
+            options={{ workerSrc: "/pdf.worker.js" }}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            <Page
+              pageNumber={pageNumber}
+              style={{
+                height: "100vh",
+                width: "100%",
+              }}
+            />
+          </Document>
+        </Grid>
+      </Grid>
+    </>
   );
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,26 +3,20 @@ import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Link from "@material-ui/core/Link";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import ResumeCard from "../components/ResumeCard";
 import { mainListItems } from "./listitems.js";
 import ResumePDF from "../images/Resume.pdf";
 import AllPagesPDFViewer from "../components/ResumePDF";
-
+import '../index.css';
 
 const drawerWidth = 240;
 
@@ -172,13 +166,11 @@ export default function Portfolio() {
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={12}>
-            <div className="Resume">
-              <div className="all-page-container">
-                <AllPagesPDFViewer pdf={ResumePDF} />
-              </div>
-
-              <hr />
-            </div>
+            {/* <div className="Resume">
+              <div className="all-page-container"> */}
+                <AllPagesPDFViewer style={{width: "100%"}} pdf={ResumePDF} />
+              {/* </div>
+            </div> */}
           </Grid>
         </Container>
       </main>
